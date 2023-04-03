270e1aadf8709f71a4cc522df85e78aec2f08947
Danila Guseynov <88967903+grechaa82@users.noreply.github.com>
2023-04-03 06:48:23 -0700

    Create Query.sql

---
+++ Query.sql
@@ -0,0 +1,6 @@
+SELECT [Product].[name], [Category].[name]
+FROM Product
+LEFT JOIN [ProductCategory]
+	ON [Product].[id] = [ProductCategory].[productId]
+LEFT JOIN [Category]
+	ON [Category].[id] = [ProductCategory].[categoryId]
